8167acdc0875663793d9318535c17bb43c73731e
dev_ElderLab <enderlabo@gmail.com>
2021-07-04 18:47:12 -0500

    Initial Commit

---
+++ EcommerceUI-DrapShop/EcommerceUI_DrapShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceUI_DrapShopApp.swift
+//  EcommerceUI-DrapShop
+//
+//  Created by Laborit on 4/07/21.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceUI_DrapShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
